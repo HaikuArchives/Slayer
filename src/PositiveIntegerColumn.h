38357b19db22b6ce74416e5eea6aa8eb159c684b
Emir SARI <bitigchi@me.com>
2024-03-17 17:45:00 +0300

    Align values right for a more organised display

--- src/PositiveIntegerColumn.h
+++ src/PositiveIntegerColumn.h
@@ -7,7 +7,7 @@ class PositiveIntegerColumn : public BIntegerColumn {
   public:
 	PositiveIntegerColumn(
 		const char *title, float width, float minWidth, float maxWidth,
-		alignment align = B_ALIGN_LEFT
+		alignment align = B_ALIGN_RIGHT
 	);
 	virtual void DrawField(BField *field, BRect rect, BView *parent);
 };
